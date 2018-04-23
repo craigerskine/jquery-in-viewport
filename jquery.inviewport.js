74088e42a813be01b00b39b1749c37061ead7c53
Craig Erskine <craig@craigerskine.com>
2018-04-23 10:46:05 -0500

    added example page

--- jquery.inviewport.js
+++ jquery.inviewport.js
@@ -1,11 +1,11 @@
 // inViewport 1.0.0
 (function($){
-  $.fn.inViewport = function(paramOffset){
-    var elOffset = ~~Math.round(parseFloat(paramOffset)) || 0;
-    var elTop = $(this).offset().top;
-    var elBot = elTop + $(this).outerHeight();
-    var viewTop = $(window).scrollTop();
-    var viewBot = viewTop + ($(window).height() + elOffset);
-    return elBot > viewTop && elTop < viewBot;
+  $.fn.inViewport = function(offsetVh){
+    var elOffsetVh = ~~Math.round(parseFloat(offsetVh)) || 0;
+    var elT = $(this).offset().top;
+    var elB = elT + $(this).outerHeight();
+    var viewT = $(window).scrollTop();
+    var viewB = viewT + ($(window).height() + elOffsetVh);
+    return elB > viewT && elT < viewB;
   };
 }(jQuery));
